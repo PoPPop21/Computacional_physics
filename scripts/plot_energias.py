cbc86b417f3261f9c98f059e4c6ecf31852369d4
PoPPop21 <sofi@avengerkittyscomputer.localdomain>
2025-06-28 19:28:24 -0500

    Agrego el calculo y el script para graficar las energias del sistema

---
+++ scripts/plot_energias.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt("results/energias.dat")
+t = data[:, 0]
+Ek = data[:, 1]
+Ep = data[:, 2]
+Et = data[:, 3]
+
+plt.plot(t, Ek, label="Cinética")
+plt.plot(t, Ep, label="Potencial")
+plt.plot(t, Et, label="Total", linestyle='--')
+plt.xlabel("Tiempo")
+plt.ylabel("Energía")
+plt.title("Energías del sistema")
+plt.legend()
+plt.grid()
+plt.show()
