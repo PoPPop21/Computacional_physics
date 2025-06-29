4c28aaee1d9da74dd7871fb31821d75cdff1b943
PoPPop21 <sofi@avengerkittyscomputer.localdomain>
2025-06-29 12:51:21 -0500

    Actualizo Cuerpo.h, Cuerpo.cpp y main.cpp

--- include/Cuerpo.h
+++ include/Cuerpo.h
@@ -1,3 +1,8 @@
+/**
+* @author Angela Sofia Malagon Palmino & Jhousua Sebastian Albadan Infante
+* @version No se, hicimos varias :D
+* @date 2025-07-03
+*/
 #ifndef CUERPO_H
 #define CUERPO_H
 
@@ -7,14 +12,40 @@ using namespace Eigen;
 using std::vector;
 
 /**
-* Clase que representa un cuerpo celeste en el sistema gravitacional.
+* @class Cuerpo
+* @brief Representa un cuerpo celeste en un sistema gravitacional.
 */
 
 class Cuerpo {
+/**
+* Private:
+* @param m Masa del cuerpo.
+* @param R Radio del cuerpo.
+* @param r Vector3d que representa la posición del cuerpo.
+* @param V Vector3d que representa la velocidad del cuerpo.
+* @param F Vector3d que representa la fuerza neta actuando sobre el cuerpo.
+*/
 private:
     double m, R;
     Vector3d r, V, F; // Posición, velocidad y fuerza del cuerpo
-
+/**
+* Public:
+* @brief Métodos para inicializar el cuerpo, calcular fuerzas, mover posiciones y velocidades.
+* @details
+* Inicie: Inicializa el cuerpo con masa, radio, posición y velocidad.
+* GetF: Devuelve la fuerza neta actuando sobre el cuerpo.
+* BorreFuerza: Resetea la fuerza neta a cero.
+* AdicioneFuerzaGravitacional: Calcula y añade la fuerza gravitacional de otro cuerpo.
+* MuevaR: Actualiza la posición del cuerpo según su velocidad y la fuerza neta.
+* MuevaV: Actualiza la velocidad del cuerpo usando el método de Verlet.
+* Getx, Gety, Getz: Devuelven las coordenadas x, y, z de la posición del cuerpo.
+* GetVx, GetVy, GetVz: Devuelven las componentes de la velocidad del cuerpo.
+* GetM: Devuelve la masa del cuerpo.
+* GetR: Devuelve el radio del cuerpo.
+* AddForce: Añade una fuerza adicional al cuerpo.
+* GetPos: Devuelve la posición del cuerpo como un Vector3d.
+* @note Las fuerzas se calculan en unidades el sistema internacional, y la constante G se define en constantes.h.
+*/
 public:
     void Inicie(double m0, double R0, const Vector3d &r0, const Vector3d &V0);
     Vector3d GetF() const { return F; }
@@ -23,8 +54,6 @@ public:
     void MuevaR(double dt);
     void MuevaV(double dt, const Vector3d &F_antes);
     //Calculamos la energía
-    //void CalcEnergia(const vector<Cuerpo> &cuerpos, double &Ek, double &Ep, double G);
-
     // Getters
     double Getx() const { return r.x(); }
     double Gety() const { return r.y(); }
